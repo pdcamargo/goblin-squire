f04f2d5d0cdf4232df44f548d1acd2cc17033a21
Patrick Dias <pdcamargo@hotmail.com>
2024-11-13 12:50:34 -0300

    Add editorconfig and create package engine with core, server and client exports

---
+++ packages/engine/.eslintrc.js
@@ -0,0 +1,10 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ["@repo/eslint-config/library.js"],
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: ["*.json"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+};
